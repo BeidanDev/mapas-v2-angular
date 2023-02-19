4030b2bad92a5231124485e7e98d1954e28080b3
BeidanDev <franco.peralta7@hotmail.com>
2023-02-18 21:45:01 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl';
+
+Mapboxgl.accessToken = '';
+
+if(!navigator.geolocation) {
+  alert('Navegador no soporta la Geolocation');
+  throw new Error('Navegador no soporta la Geolocation');
+}
+
 if (environment.production) {
   enableProdMode();
 }
